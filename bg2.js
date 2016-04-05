3eb40e0c3b040ab3864193768f56776b9e390a39
kg <kjg310@gmail.com>
2016-04-05 10:35:43 -0400

    Working on race/class min/max stats

--- bg2.js
+++ bg2.js
@@ -1,6 +1,26 @@
 // var d6 = function() {
 // 	Math.floor(Math.random()*6 + 1) 
 // }
+var race = function(name, minStr, maxStr, minDex, maxDex, minCon, maxCon, minInt, maxInt, minWis, maxWis, minCha, maxCha) {
+	this.name = name;
+	this.minStr = minStr;
+	this.maxStr = maxStr;
+	this.minDex = minDex;
+	this.maxDex = maxDex;
+	this.minCon = maxCon;
+	this.maxCon = maxCon;
+	this.minInt = minInt;
+	this.maxInt = maxInt;
+	this.minWis = minWis;
+	this.maxWis = maxWis;
+	this.minCha = minCha;
+	this.maxCha = maxCha;
+}
+
+var human = new race('Human', 3, 18, 3, 18, 3, 18, 3, 18, 3, 18, 3, 18)
+var elf = new race('Elf', 3, 18, 7, 19, 6, 17, 8, 18, 3, 18, 8, 18)
+
+
 var character = {}
 
 var generateStats = function() {
@@ -32,11 +52,29 @@ var generateStats = function() {
 
 	}
 
-	
-
 }
+
+$("#chooseRace").on('change', function() {
+	// alert($("#chooseRace").val())
+	character.race = $("#chooseRace").val();
+	switch($("#chooseRace").val()) {
+		case 'Human':
+			character.race = human;
+			break;
+		case 'Elf': 
+			character.race = elf;
+			break;
+		default:
+			break;
+	}
+	$("#raceDisplay").text( character.race.name )
+})
 
 $("#reRoll").click(function() {
 	generateStats();
 })
 
+$("input:radio[name=gender]").click(function() {
+	character.gender = $(this).val()
+	$("#genderDisplay").text(character.gender)
+});
